f553d28a0bd12bf68a13ebf00fb7a5ec3d18e096
sergei.krumin <sergei.krumin@netcosports.com>
2018-09-14 17:12:10 +0300

    Get localization for current bundle.

--- JSQMessagesViewController/Categories/NSBundle+JSQMessages.m
+++ JSQMessagesViewController/Categories/NSBundle+JSQMessages.m
@@ -20,6 +20,89 @@
 
 #import "JSQMessagesViewController.h"
 
+typedef enum JSQLanguageValue {
+    JSQLanguageValueEnglish = 0,
+    JSQLanguageValueFrench = 1
+} JSQLanguageValue;
+
+@interface JSQLanguage : NSObject
+@property (nonatomic, assign) JSQLanguageValue languageType;
+@end
+
+@implementation JSQLanguage : NSObject
+
+- (instancetype)initWithLanguageType:(JSQLanguageValue)languageType
+{
+    self = [super init];
+    if (self) {
+        self.languageType = languageType;
+    }
+    return self;
+}
+
+- (NSString *)code
+{
+    if (self.languageType == JSQLanguageValueEnglish) {
+        return @"en";
+    } else if (self.languageType == JSQLanguageValueFrench) {
+        return @"fr";
+    } else {
+        return @"en";
+    }
+}
+
+@end
+
+
+static NSString * const JSQLanguageManagerSavedLanguageKey = @"savedLanguage";
+
+@implementation JSQLanguageManager : NSObject
+
++ (JSQLanguage *)language
+{
+    NSUserDefaults *defaults = NSUserDefaults.standardUserDefaults;
+    JSQLanguage *userLanguage = [self defaultLanguage];
+    
+    NSString *savedLanguageCode = [defaults objectForKey:JSQLanguageManagerSavedLanguageKey];
+    if (savedLanguageCode != nil && savedLanguageCode.length > 0) {
+        for (JSQLanguage* languageObject in [self avaibleLanguages]) {
+            if ([[languageObject code] isEqualToString:savedLanguageCode]) {
+                userLanguage = languageObject;
+                break;
+            }
+        }
+    }
+    return userLanguage;
+}
+
++ (NSArray *)avaibleLanguages
+{
+    JSQLanguage *frenchLanguage = [[JSQLanguage alloc] initWithLanguageType:JSQLanguageValueFrench];
+    JSQLanguage *englishLanguage = [[JSQLanguage alloc] initWithLanguageType:JSQLanguageValueEnglish];
+    return @[englishLanguage, frenchLanguage];
+}
+
++ (JSQLanguage *)defaultLanguage
+{
+    JSQLanguage *englishLanguage = [[JSQLanguage alloc] initWithLanguageType:JSQLanguageValueEnglish];
+    return englishLanguage;
+}
+
++ (NSBundle *)bundle
+{
+    NSBundle *defaultBundle = NSBundle.mainBundle;
+    NSString *bundleCode = [[self language] code];
+    NSString *bundleResourcePath = [[NSBundle mainBundle] pathForResource:bundleCode ofType:@"lproj"];
+    if (bundleResourcePath == nil) {
+        return defaultBundle;
+    }
+    
+    return [[NSBundle alloc] initWithPath:bundleResourcePath];
+}
+
+@end
+
+
 @implementation NSBundle (JSQMessages)
 
 + (NSBundle *)jsq_messagesBundle
@@ -30,13 +113,25 @@
 + (NSBundle *)jsq_messagesAssetBundle
 {
     NSString *bundleResourcePath = [NSBundle jsq_messagesBundle].resourcePath;
+ 
+    NSString *bundleCode = [[JSQLanguageManager language] code];
+    
     NSString *assetPath = [bundleResourcePath stringByAppendingPathComponent:@"JSQMessagesAssets.bundle"];
-    return [NSBundle bundleWithPath:assetPath];
+    
+    NSBundle *bundle = [NSBundle bundleWithPath:assetPath];
+    
+    NSString *bundleResourcePath = [bundle pathForResource:bundleCode ofType:@"lproj"];
+    
+    return [[NSBundle alloc] initWithPath:bundleResourcePath];
 }
 
 + (NSString *)jsq_localizedStringForKey:(NSString *)key
 {
-    return NSLocalizedStringFromTableInBundle(key, @"JSQMessages", [NSBundle jsq_messagesAssetBundle], nil);
+    NSBundle *bundle = [NSBundle jsq_messagesAssetBundle];
+    NSString *format = NSLocalizedStringFromTableInBundle(key, @"JSQMessages", bundle, nil);
+    NSString *string = [[NSString alloc] initWithFormat:format locale:NSLocale.currentLocale];
+    NSLog(@"localized: %@", string);
+    return string;
 }
 
 @end
